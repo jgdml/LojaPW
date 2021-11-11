ce5b8d3b0390890cca367100a3d9176c66aea621
jgdml <jgdml2801@gmail.com>
2021-11-11 18:28:15 -0300

    Contagem

--- src/main/java/com/jg/lojapw/controller/HomeController.java
+++ src/main/java/com/jg/lojapw/controller/HomeController.java
@@ -1,13 +1,34 @@
 package com.jg.lojapw.controller;
 
+import com.jg.lojapw.repo.CidadeRepo;
+import com.jg.lojapw.repo.FuncionarioRepo;
+import com.jg.lojapw.repo.ProdutoRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    private ProdutoRepo produtoRepo;
+
+    @Autowired
+    private FuncionarioRepo funcionarioRepo;
+
+    @Autowired
+    private CidadeRepo cidadeRepo;
+
     @GetMapping("/administrativo")
-    public String acessarHome(){
-        return "administrativo/home";
+    public ModelAndView acessarHome(){
+        ModelAndView mv = new ModelAndView("administrativo/home");
+
+        mv.addObject("prod" ,produtoRepo.count());
+        mv.addObject("func" ,funcionarioRepo.count());
+        mv.addObject("cid" ,cidadeRepo.count());
+
+
+        return mv;
     }
 }
